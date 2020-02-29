ba755d3d365718a675d667fc717cb1422f1cae7d
Ubuntu <ubuntu@ip-172-31-31-243.us-east-2.compute.internal>
2020-02-29 04:04:24 +0000

    created map popup to show during map load.

--- script.js
+++ script.js
@@ -12,13 +12,7 @@ var map = new mapboxgl.Map({
   zoom: 11
 });
 
-
-map.on('click', function(e) {
-  console.log("click")
-  
-})
-
-function checkAPI(){
+function twentyfourHourForecast(){
   axios.get(
       API_URL+"environment/24-hour-weather-forecast",{
         params:{
@@ -31,3 +25,18 @@ function checkAPI(){
     console.log(response.data.api_info.status);
   })
 }
+
+let coordinates = [103.8198, 1.3521]
+let description = "Hello there"
+
+map.on('click', function(e) {
+  console.log("click")
+  
+})
+
+map.on('load', function(){
+  new mapboxgl.Popup()
+  .setLngLat(coordinates)
+  .setHTML(description)
+  .addTo(map);
+})
