da3fcd098eb614d27954e760c5ea825633fb1a15
Ubuntu <ubuntu@ip-172-31-31-243.us-east-2.compute.internal>
2020-02-29 03:49:21 +0000

    added axios and API. API returns response successfully.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /* global mapboxgl */
+/* global axios */
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZXVnZW5ldGVvMTk4OCIsImEiOiJjazc2MnBrem4wYWo0M2VvenNzcmcxNHkwIn0.vTWAuusBOsObe6-hKtokBg';
+let API_URL = "https://api.data.gov.sg/v1/"
+
 
 var map = new mapboxgl.Map({
   container: 'map',
@@ -12,4 +15,19 @@ var map = new mapboxgl.Map({
 
 map.on('click', function(e) {
   console.log("click")
+  
 })
+
+function checkAPI(){
+  axios.get(
+      API_URL+"environment/24-hour-weather-forecast",{
+        params:{
+          "date":"2020-02-29"
+        }
+        
+      }
+    ).then(function(response){
+    console.log("API Responded");
+    console.log(response.data.api_info.status);
+  })
+}
