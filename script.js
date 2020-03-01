6368f492b7db0961276e0b5435eea4b6b6cd08a9
Ubuntu <ubuntu@ip-172-31-31-243.us-east-2.compute.internal>
2020-03-01 04:14:28 +0000

    added icon on map.

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ var map = new mapboxgl.Map({
   zoom: 11
 });
 
-function twentyfourHourForecast(){
+function twentyfourHourForecast() {
   axios.get(
-      API_URL+"environment/24-hour-weather-forecast",{
-        params:{
-          "date":"2020-02-29"
-        }
-        
+    API_URL + "environment/24-hour-weather-forecast", {
+      params: {
+        "date": "2020-02-29"
       }
-    ).then(function(response){
+
+    }
+  ).then(function(response) {
     console.log("API Responded");
     console.log(response.data.api_info.status);
-    
+
     let description = response.data.api_info.status
-    
+
     forecastPopup(description);
-    
+
   })
 }
 
@@ -36,19 +36,47 @@ let coordinates = [103.8198, 1.3521];
 
 map.on('click', function(e) {
   console.log("click")
-  
+
 })
 
-map.on('load', function(){
-  twentyfourHourForecast();
-  
-})
+map.on('load', function() {
+  // standard Mapbox cod to load images
+  map.loadImage(
+    "https://img.icons8.com/plasticine/100/000000/rain.png",
+    function(error, image) {
+      if (error) throw error;
+      map.addImage('rain', image);
+      map.addSource('point', {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': [{
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [103.8198, 1.3521]
+            }
+          }]
+        }
+      });
+      map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'point',
+        'layout': {
+          'icon-image': 'rain',
+          'icon-size': 0.75
+        }
+      });
+    }
+  );
+});
 
-function forecastPopup(description){
+function forecastPopup(description) {
   console.log(description)
-  
+
   new mapboxgl.Popup()
-  .setLngLat(coordinates)
-  .setHTML(description)
-  .addTo(map);
+    .setLngLat(coordinates)
+    .setHTML(description)
+    .addTo(map);
 }
